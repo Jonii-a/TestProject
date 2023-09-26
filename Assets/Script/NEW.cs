7d98b49182f60eeef522cfa0c905e71f722a803b
soundvolt34@gmail.com <soundvolt34@gmail.com>
2023-09-27 00:10:10 +0900

    ろぐ

---
+++ Assets/Script/NEW.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NEW : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("NEW");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
